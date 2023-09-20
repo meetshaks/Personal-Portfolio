2f2a41b291be345a0e1dabdc475f5217701cb2ab
meetshaks <shakilahmed4024@gmail.com>
2023-09-20 11:05:10 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# HTML, CSS, JAVAScript
+
+
+# Personal-Portfolio
---
+++ mail.php
@@ -0,0 +1,12 @@
+<?php
+
+$recepient = "shakilahmed4024@gmail.com";
+$sitename = "Shakil";
+
+$name = trim($_POST["name"]);
+$email = trim($_POST["email"]);
+$text = trim($_POST["text"]);
+$message = "Name: $name \nEmail: $email \nText: $text";
+
+$pagetitle = "New message from the \"$sitename\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
